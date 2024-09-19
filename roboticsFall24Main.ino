072782b61d28b4b7c7dec75acc90edcc53d468cc
drchimes <chimes@misshalls.org>
2024-09-19 09:40:00 -0400

    Update roboticsFall24Main.ino

--- roboticsFall24Main.ino
+++ roboticsFall24Main.ino
@@ -4,19 +4,17 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
+Adafruit_DCMotor *motorLeft = AFMS.getMotor(1);
+Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
 
 void setup() {
   // put your setup code here, to run once:
 AFMS.begin();
 
-stop();
 }
 
 void loop() {
-
+  motorLeft ->setSpeed(200);
+  motorLeft ->run(FORWARD);
   
 }
